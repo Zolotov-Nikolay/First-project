23fd326c5607dbee55caedb05d11599851a6fdc3
Nikolay Zolotov <kolyabengamin@icloud.com>
2021-03-31 17:31:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++; //+1
+// decr--; //-1
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+// console.log(2 + 2 * 2 != '6');
+
+// const isChecked = false,
+//       isClose = true;
+
+// console.log(isChecked || !isClose);
+
+//  const category = 'toys';
+
+//  console.log(`https://someurl.com/${category}/5`);
+
+//  const user = "Ivan";
+
+//  alert(`Привет, ${user}`);
+
+//alert('Hello');
+
+//const result = confirm("Are you here?")
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18")
+//console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваша фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// document.write(answers);
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// //console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     imya: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// //console.log(obj.imya);
+// console.log(obj ["imya"]);
+
+//         //   0           1           2   3           4
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
