25b5312d91da3a261de053d91eb7126482200628
Nikolay Zolotov <kolyabengamin@icloud.com>
2021-04-06 18:27:29 +0300

    save

--- jscript!.js
+++ jscript!.js
@@ -175,71 +175,138 @@
 // console.log(parseInt(Test));
 // console.log(parseFloat(Test));
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
+// start();
 
-start();
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt ('Один из последних просмотренных фильмов?', ''),
-              b = prompt ('На сколько оцените его?', '');
+// function rememberMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt ('Один из последних просмотренных фильмов?', ''),
+//               b = prompt ('На сколько оцените его?', '');
     
-        if (a != null && b != null && a!= '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
+//         if (a != null && b != null && a!= '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+//     }
+// }
 
 // rememberMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов!');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель!');
-    } else if (personalMovieDB.count > 30) {
-        console.log('Вы настоящий кинмоман!');
-    } else {
-        console.log('Произошла ошибка!');
-    }
-}
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         console.log('Просмотрено довольно мало фильмов!');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log('Вы классический зритель!');
+//     } else if (personalMovieDB.count > 30) {
+//         console.log('Вы настоящий кинмоман!');
+//     } else {
+//         console.log('Произошла ошибка!');
+//     }
+// }
 
 // detectPersonalLevel();
 
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
+// function showMyDB (hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for (let i = 1; i <= 3; i++) {
+//         const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//         personalMovieDB.genres[i - 1] = genre;
+//         // personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`); можно записывать так
+//     }
+// }
+// writeYourGenres();
+
+// function first() {
+//     // Do something
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошёл этот урок!');
+// }
+
+// learnJS('JavaScript', done);
+
+// learnJS('JavaScript', function() {
+//     console.log('Я прошёл этот урок!');
+// });
+
+const options = {
+    NAME: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
     }
-}
+};
 
-showMyDB(personalMovieDB.privat);
+options.makeTest();
 
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres[i - 1] = genre;
-        // personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`); можно записывать таким образом
-    }
-}
+const {border, bg} = options.colors;
+console.log(border, bg);
 
-writeYourGenres();
+// console.log(Object.keys(options).length);//!!!количество элементов в объекте!!!
+
+// console.log(options.NAME);
+
+// delete options.NAME;
+
+// console.log(options);
+
+// let counter = 0;
+// for ( let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for ( let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
